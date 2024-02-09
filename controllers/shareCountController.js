e8446040d62988afa502e307b5e54505077aeffb
Jagadeeshpavan <120124386+Jagadeeshpavan@users.noreply.github.com>
2024-02-09 08:31:27 +0530

    oikjh

--- controllers/shareCountController.js
+++ controllers/shareCountController.js
@@ -1,37 +1,13 @@
-// File: controllers/shareCountController.js
+// controllers/shareController.js
 
-const ShareCount = require('../models/ShareCount');
-const shareCountService = require('../services/shareCountService');
+const Share = require('../models/ShareCount');
 
-const receiveShareCounts = async (req, res) => {
-    try {
-        const { postId, facebookShareCount, twitterShareCount, whatsAppShareCount, linkedInShareCount, emailShareCount } = req.body;
+const shareController = {};
 
-        // Create a ShareCount object
-        const shareCount = new ShareCount(postId, facebookShareCount, twitterShareCount, whatsAppShareCount, linkedInShareCount, emailShareCount);
-
-        // Call the service to save the share counts
-        await shareCountService.saveShareCounts(shareCount);
-
-        res.status(200).send('Share counts received and processed successfully');
-    } catch (error) {
-        console.error('Error receiving share counts:', error);
-        res.status(500).send('Internal server error');
-    }
+shareController.sharePost = (req, res) => {
+    const postId = req.body.postId;
+    const socialMedia = req.params.socialMedia;
+ res.json({ success: true, message: `${socialMedia} share successful for post ID ${postId}` });
 };
-const getShareCounts = async (req, res) => {
-    try {
-      const { postId } = req.params;
-      // Logic to retrieve share counts from the database or other storage
-      // For demonstration purposes, assuming share counts are fetched from a database
-      const shareCounts = await ShareCountModel.findOne({ postId });
-      res.status(200).json(shareCounts);
-    } catch (error) {
-      console.error('Error retrieving share counts:', error);
-      res.status(500).send('Internal server error');
-    }
-  };
-module.exports = {
-    receiveShareCounts,
-    getShareCounts 
-};
+
+module.exports = shareController;
--- models/ShareCount.js
+++ models/ShareCount.js
@@ -1,13 +1,3 @@
+const Share = {};
 
-class ShareCount {
-    constructor(postId, facebookShareCount, twitterShareCount, whatsAppShareCount, linkedInShareCount, emailShareCount) {
-        this.postId = postId;
-        this.facebookShareCount = facebookShareCount;
-        this.twitterShareCount = twitterShareCount;
-        this.whatsAppShareCount = whatsAppShareCount;
-        this.linkedInShareCount = linkedInShareCount;
-        this.emailShareCount = emailShareCount;
-    }
-}
-
-module.exports = ShareCount;
+module.exports = Share;
